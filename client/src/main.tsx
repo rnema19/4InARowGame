d90b0991920b9e07c84ca8a4da948eba4d931fd4
rnema19 <nemarishabh9@gmail.com>
2025-11-12 10:51:52 +0530

    react-router-dom version updated code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Toaster } from '@/components/ui/sonner'
 import Home from './pages/Home'
 import Game from './pages/Game'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/game',
+    element: <Game />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-      <Toaster />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    <Toaster />
   </React.StrictMode>,
 )
